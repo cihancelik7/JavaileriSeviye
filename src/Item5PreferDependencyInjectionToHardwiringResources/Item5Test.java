189aaa2d4bbebf3a51af147a4ed6ba76e059c6b9
Cihan0201 <114297546+Cihan0201@users.noreply.github.com>
2024-02-25 00:49:11 +0300

    DependencyInjection

---
+++ src/Item5PreferDependencyInjectionToHardwiringResources/Item5Test.java
@@ -0,0 +1,7 @@
+package Item5PreferDependencyInjectionToHardwiringResources;
+
+public class Item5Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Item5PreferDependencyInjectionToHardwiringResources/Sozluk.java
@@ -0,0 +1,11 @@
+package Item5PreferDependencyInjectionToHardwiringResources;
+
+public class Sozluk {
+    private String dil;
+    public Sozluk(String dil){
+        this.dil = dil;
+    }
+    public String toString(){
+        return dil;
+    }
+}
